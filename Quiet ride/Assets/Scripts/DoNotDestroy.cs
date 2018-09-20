ae24f06565ea753fe9a55bc53c91a7d8fb42657f
Daniel Jardine <1005730@student.sae.edu.au>
2018-09-21 00:51:43 +1000

    Mostly caught back up
    Very basic scene reconstruction

---
+++ Quiet ride/Assets/Scripts/DoNotDestroy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoNotDestroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        DontDestroyOnLoad(this.gameObject);
+	}
+	
+}
